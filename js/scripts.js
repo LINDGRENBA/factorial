1fb95e4b6bbf986ee301134a80452ca97d924786
Brittany Lindgren <brittdevelopsit@gmail.com>
2020-05-13 15:30:12 -0700

    Update scripts to include for loop, take in user number and return factorial.

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,21 @@
 // function to do business logic
 let factorial = function(initialNumber, multiplyByN) {
   if (initialNumber > 0) {
-    return true;
+    // if initialNumber is greater than zero, run the loop
+    for (let index = 1; multiplyByN > 1; index += 1) {
+      multiplyByN -= 1;
+      let number = initialNumber * multiplyByN;
+      initialNumber = number;
+    }
+    // and THEN return the result of the loop
+    return initialNumber;
+    // need to have return outside of for loop or it will stop the loop
   } else if (initialNumber === 0) {
-  return 1;
+    return 1;
   } else {
-  return false;
+    return false; 
   }
 }
-
 
 // jQuery user interface logic
 $(document).ready(function() {
@@ -18,7 +25,17 @@ $(document).ready(function() {
     let initialNumber = parseInt($("input#number-input").val());
     let multiplyByN = parseInt($("input#number-input").val());
     let result = factorial(initialNumber, multiplyByN);
-    $(".final-number").text(result);
+    $(".number-input").text(result);
+
+    // $(".number-input").text(initialNumber);
+
+    // if (!result) {
+    //   $(".final-number").text("N/A");
+    // } else {
+    //   (".final-number").text(result);
+    // }
+
+
 
   });
 });
