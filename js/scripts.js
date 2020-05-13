63d2c469d41c8a81c1ec1c7555c7dcaddb8a1aa6
Brittany Lindgren <brittdevelopsit@gmail.com>
2020-05-13 12:28:36 -0700

    Update html result div, write inital script to return false when any number is submitted.

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,7 @@
-// let n = 5;
+$(document).ready(function() {
+  $(".factorial").submit(function(event) {
+    event.preventDefault();
 
-// for (let i = n; i > 0; i -= 1) {
-//   a = n*(i-1); // 120
-//   console.log(a);
-// }
-
-// for (let i = n; i > 0; i -= 1) {
-//   let multiplyBy = n;
-//   multiplyBy -= 1;
-//   n = n*multiplyBy;
-//   console.log(n);
-// }
-
-let factorial = function(a, b) {
-  for(i = a; i > 0; i -= 1) {
-    b = b - 1;
-    let result = a * b;
-  }
-}
-
-factorial(5, 5);
-$(".result").text(result);
+    $(".final-number").text(false);
+  });
+});
